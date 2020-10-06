9951b664efbc77f8defa48999d770a18a210061c
Irina Kramer <irakramer@gmail.com>
2020-10-06 12:48:46 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lowepro Quickfinder
+
+Lowepro Quickfinder is a Single Page App for quickly navigating to the right camera bag collection.  I designed UI/UX, developed prototype and programmed the app. I used lightweight MVC framework SPApp, jQuery, custom SVG icons, CSS and HTML, and integrated in PHP environment.
---
+++ js/data.js
@@ -0,0 +1,324 @@
+// Bag items objects
+var Adventura = {
+  "name":"Adventura",
+  "description":"Protection and practicality in a modern compact camera bag in 9 sizes to fit your needs.",
+  "image":"adventura.jpg",
+  "url":"//www.lowepro.com/adventura",
+  "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Adventura');"
+};
+
+var Dashpoint = {
+  "name":"Dashpoint",
+  "description":"Compact protection for small cameras and devices such as GoPro action video cameras & accessories.",
+  "image":"dashpoint.jpg",
+  "url":"//www.lowepro.com/dashpoint",
+  "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Dashpoint');"
+};
+
+var DroneGuard = {
+  "name":"DroneGuard",
+  "description":"Drone packs and cases purpose-built to carry your imaging quadcopter safely to your flight location and back.",
+  "image":"droneguard.jpg",
+  "url":"//www.lowepro.com/droneguard",
+  "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'DroneGuard');"
+};
+var DroneGuardInspired = {
+  "name":"DroneGuard Pro Inspired",
+  "description":"Professional drone backpack made specifically for DJI Inspire I & II plus transmitter, batteries, tools and accessories.",
+  "image":"droneguard-inspired.jpg",
+  "url":"//www.lowepro.com/droneguard",
+  "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'DroneGuard Pro Inspired');"
+};
+var DroneGuardMavic = {
+  "name":"DroneGuard for Mavic",
+  "description":"Drone backpacks and cases with impact protection designed for DJI Mavic Pro plus essentials.",
+  "image":"droneguard-mavic.jpg",
+  "url":"//www.lowepro.com/droneguard",
+  "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'DroneGuard for Mavic');"
+};
+var DroneGuardPhantom = {
+  "name":"DroneGuard for Phantom",
+  "description":"Professional-grade backpacks designed to comfortably carry and protect a DJI Phantom quadcopter and more.",
+  "image":"droneguard-phantom.jpg",
+  "url":"//www.lowepro.com/droneguard",
+  "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'DroneGuard Phantom');"
+};
+
+var DryZone = {
+  "name":"DryZone",
+  "description":"Durable, waterproof camera bags for photographers who need ultimate weather and water protection.",
+  "image":"dryzone.jpg",
+  "url":"//www.lowepro.com/dryzone",
+  "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'DryZone');"
+};
+
+var Fastpack = {
+  "name":"Fastpack",
+  "description":"Travel smart with your camera, laptop, tablet & more. Fastpack keeps you ready for the pace of modern travel.",
+  "image":"fastpack.jpg",
+  "url":"//www.lowepro.com/fastpack",
+  "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Fastpack');"
+};
+
+var Flipside = {
+  "name":"Flipside",
+  "description":"High-capacity camera backpacks with body-side access designed to protect and carry a full kit of camera equipment.",
+  "image":"flipside.jpg",
+  "url":"//www.lowepro.com/flipside",
+  "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Flipside');"
+};
+
+var FlipsideTrek = {
+  "name":"Flipside Trek",
+  "description":"Outdoor camera backpack with split compartment for both camera and non-camera gear.",
+  "image":"flipsidetrek.jpg",
+  "url":"//www.lowepro.com/flipside",
+  "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Flipside Trek');"
+};
+
+var GearUp = {
+  "name":"GearUp",
+  "description":"Simplify and organize your cords, cables, chargers and small devices with a range of smart packing solutions.",
+  "image":"gearup.jpg",
+  "url":"//www.lowepro.com/gearup",
+  "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'GearUp');"
+};
+
+var Hatchback = {
+   "name":"Photo Hatchback",
+   "description":"Convertible daypacks with removable camera box and plenty of space for personal items.",
+   "image":"photohatchback.jpg",
+   "url":"//www.lowepro.com/photo-hatchback-aw",
+   "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Photo Hatchback');"
+ };
+
+var Highline = {
+   "name":"Highline",
+   "description":"Rugged yet refined laptop  travel packs & rolling luggage that are equally at home in the outback or a 5-star hotel.",
+   "image":"highline.jpg",
+   "url":"//www.lowepro.com/highline",
+   "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Highline');"
+ };
+
+ var Magnum = {
+    "name":"Magnum",
+    "description":"Super high-capacity professional camera shoulder bags designed to carry and protect the tools of your trade.",
+    "image":"placeholder.jpg",
+    "url":"//www.lowepro.com/magnum",
+    "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Magnum');"
+  };
+
+  var mTrekker = {
+     "name":"m-Trekker",
+     "description":"Mirrorless camera bags with a classic design, modern textiles and the features you need.",
+     "image":"mtrekker.jpg",
+     "url":"//www.lowepro.com/m-trekker",
+     "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'm-Trekker');"
+   };
+
+var Nova =  {
+   "name":"Nova II",
+   "description":"Protective, Customizable, Dependable. Our iconic camera bag keeps evolving and getting better.",
+   "image":"nova.jpg",
+   "url":"//www.lowepro.com/nova",
+   "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Nova');"
+ };
+
+var Passport = {
+  "name":"Passport",
+  "description":"Travel ready and designed for fast and flexible access for your camera and your personal gear.",
+  "image":"passport.jpg",
+  "url":"//www.lowepro.com/passport-series",
+  "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Passport');"
+};
+
+var PhotoClassic = {
+   "name":"Photo Classic",
+   "description":"Protective high-capacity camera pack with premium features and exceptional value.",
+   "image":"photoclassic.jpg",
+   "url":"//www.lowepro.com/photo-classic",
+   "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Photo Classic');"
+ };
+
+var PhotoSport = {
+   "name":"Photo Sport",
+   "description":"Ultra-lightweight photo pack for adventure sports and active pursuits. Go fast and light with your gear!",
+   "image":"photosport.jpg",
+   "url":"//www.lowepro.com/photosport",
+   "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Photo Sport');"
+ };
+
+ var PhotoStream = {
+    "name":"PhotoStream",
+    "description":"Lightweight & armored, carry-on compatible camera rolling cases in 2-wheel and 4-wheel spinner models.",
+    "image":"photostream.jpg",
+    "url":"//www.lowepro.com/photostream",
+    "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Photo Classic');"
+  };
+
+var ProRoller = {
+   "name":"Pro Roller",
+   "description":"Professional rolling camera bags in 3 sizes for traveling photographers who must carry a wide range of gear on-location.",
+   "image":"proroller.jpg",
+   "url":"//www.lowepro.com/pro-roller-x-aw",
+   "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Pro Roller');"
+ };
+
+ var ProRunner = {
+    "name":"Pro Runner",
+    "description":"Get on-location with all your essential tools in the pro photographer's go-to camera packs including a hybrid rolling backpack.",
+    "image":"prorunner.jpg",
+    "url":"//www.lowepro.com/prorunner",
+    "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Pro Runner');"
+  };
+
+ var ProTactic = {
+    "name":"ProTactic",
+    "description":"High-performance, urban-inspired, professional camera backpacks and shoulder bags with 4 ways to access your gear.",
+    "image":"protactic.jpg",
+    "url":"//www.lowepro.com/protactic",
+    "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'ProTactic');"
+  };
+
+  var ProTrekker = {
+     "name":"Pro Trekker",
+     "description":"The ultimate, big-glass expedition camera backpacks built to deliver uncompromising protection and comfort in the field.",
+     "image":"protrekker.jpg",
+     "url":"//www.lowepro.com/trekker",
+     "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Pro Trekker');"
+   };
+
+var QuadGuard = {
+    "name":"QuadGuard",
+    "description":"FPV racing drone backpacks and cases built to carry, organize and protect FPV quads and accessories.",
+    "image":"quadguard.jpg",
+    "url":"//www.lowepro.com/quadguard",
+    "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'QuadGuard');"
+};
+
+var Ridgeline = {
+    "name":"RidgeLine",
+    "description":"Inspired by the outdoors, at home in the city. This ultimate laptop daypack features extraordinary device protection everywhere.",
+    "image":"ridgeline.jpg",
+    "url":"//www.lowepro.com/ridgeline",
+    "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'RidgeLine');"
+};
+
+var Sleevepack = {
+    "name":"SleevePack",
+    "description":"Ultralight packable laptop sleeve that converts to a lightweight laptop travel backpack.",
+    "image":"sleevepack.jpg",
+    "url":"http://store.lowepro.com/shop/laptop/sleevepack-13",
+    "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'SleevePack');"
+};
+
+var Slingshot =  {
+   "name":"Slingshot Edge",
+   "description":"Slim profile camera sling bag perfect for city streets, day trips and any time you want to keep things light, fast and easy.",
+   "image":"slingshot.jpg",
+   "url":"//www.lowepro.com/slingshot",
+   "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'SlingShot Edge');"
+ };
+
+ var Streetline = {
+    "name":"StreetLine",
+    "description":"Sophisticated bags with fold-away protection for camera and other devices plus space to protect gear you carry every day.",
+    "image":"streetline.jpg",
+    "url":"//www.lowepro.com/streetline",
+    "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'StreetLine');"
+  };
+
+var StreetAndField = {
+   "name":"Street & Field",
+   "description":"Modular carrying system that delivers freedom of movement, fast access and flexibility to professional photographers.",
+   "image":"placeholder.jpg",
+   "url":"//www.lowepro.com/sfseries",
+   "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Street & Field');"
+ };
+
+var Tahoe = {
+   "name":"Tahoe",
+   "description":"Lightweight, sporty and adaptable. Tahoe backpacks and cases are perfect for the photo hobbyist.",
+   "image":"tahoe.jpg",
+   "url":"//www.lowepro.com/tahoe",
+   "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Tahoe');"
+ };
+
+var Toploader = {
+   "name":"Toploader ",
+   "description":"Carry and protect your essential camera kit in a super compact and versatile bag that can be carried several ways.",
+   "image":"toploader.jpg",
+   "url":"http://store.lowepro.com/catalogsearch/result/?q=toploader",
+   "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Toploader');"
+ };
+
+var Viewpoint = {
+    "name":"Viewpoint",
+    "description":"Multi-purpose pack with organization for GoPro, laptop and more.",
+    "image":"viewpoint.jpg",
+    "url":"//www.lowepro.com/viewpoint",
+    "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Viewpoint');"
+ };
+
+var Whistler = {
+    "name":"Whistler",
+    "description":"4-season outdoor adventure backpack with extreme impact, abrasion and weather protection for any climate or condition.",
+    "image":"whistler.jpg",
+    "url":"//www.lowepro.com/whistler",
+    "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Whistler');"
+ };
+
+var Urbex = {
+    "name":"Urbex",
+    "description":"Streamlined laptop backpacks with an accessory organization for creative professionals who carry a range of digital devices.",
+    "image":"urbex.jpg",
+    "url":"//www.lowepro.com/urbex",
+    "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'Urbex');"
+};
+
+var TBD = {
+    "name":"TBD",
+    "description":"Lorem ipsum etc .... ",
+    "image":"placeholder.jpg",
+    "url":"//www.lowepro.com",
+    "ga":"ga('send', 'event', 'Home_Page', 'QuickFinder', 'TBD');"
+};
+
+
+// Bag Categories
+
+var itemsCameraSmall = [
+    Adventura, Dashpoint, mTrekker, Nova, Passport, Tahoe
+];
+
+var itemsCameraPlus = [
+    Fastpack, Hatchback, Nova, Passport, PhotoSport, Slingshot
+];
+
+var itemsCameraMedium = [
+    Flipside, FlipsideTrek, Nova, PhotoClassic, PhotoStream, ProTactic
+];
+
+var itemsCameraLarge = [
+  Flipside, ProRoller, ProRunner, ProTactic, ProTrekker, PhotoStream
+];
+
+var itemsLaptop = [
+    Urbex, GearUp, Highline, Ridgeline, Sleevepack, Viewpoint
+];
+
+var itemsCreator = [
+    FlipsideTrek, PhotoSport, ProTactic, Slingshot, Toploader, Viewpoint
+];
+
+var itemsDroneImaging = [
+    DroneGuardInspired, DroneGuardPhantom, DroneGuardMavic
+];
+
+var itemsDroneRacing = [
+    QuadGuard
+];
+
+var itemsDroneMicro = [
+    Dashpoint, DroneGuard, Viewpoint
+];
